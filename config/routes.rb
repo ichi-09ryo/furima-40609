e320090fe03225c84b236ccc2e9d85a91c5bb719
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-08 14:04:20 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'posts', to: 'items#index'
+  root to: 'items#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
