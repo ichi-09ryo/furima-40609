1ce2b7362396410f34de861f17464cbbc80ff096
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-16 00:34:38 +0900

    新規登録/本人情報確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :orders
-end
+
+  validates :nickname, uniqueness: true
+  validates :email, uniqueness: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龯ー－]+\z/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龯ー－]+\z/ }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :birthday, uniqueness: true
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_15_085125) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_15_110758) do
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_15_085125) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
