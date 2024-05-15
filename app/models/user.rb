e6d0bf133a4c4326b60ec82bc2ddc3218f54714d
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-16 00:42:47 +0900

    新規登録/ユーザー情報

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  validates :nickname, uniqueness: true
-  validates :email, uniqueness: true
-  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龯ー－]+\z/ }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龯ー－]+\z/ }
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
-  validates :birthday, uniqueness: true
-  end
+  validates :nickname, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)\z/ }
+  validates :password_confirmation, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龯ー－]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龯ー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :birthday, presence: true
+end
