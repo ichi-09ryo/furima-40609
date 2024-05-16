af69ed0e825f644a8e2a79dffb027cd26279ee99
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-16 11:12:23 +0900

    hoge修正

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_15_110758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
