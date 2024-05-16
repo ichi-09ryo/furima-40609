fcf8405839c94211153f03ad38556689e0b509d3
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-16 14:50:18 +0900

    ログイン完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)\z/ }
   validates :password_confirmation, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
   validates :birthday, presence: true
-  end
-  end
+
 end
