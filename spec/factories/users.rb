149e14704b6f4aae1869322bddad7bad3832a3f3
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-16 20:38:12 +0900

    Rubocop後のエラー解消

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email                 { Faker::Internet.email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
-    last_name { '市岡' }
-    first_name { '凌介' }
-    last_name_kana { 'イチオカ' }
-    first_name_kana { 'リョウスケ' }
-    birthday { 'Faker::Date.birthday' }
+    last_name             { '市岡' }
+    first_name            { '凌介' }
+    last_name_kana        { 'イチオカ' }
+    first_name_kana       { 'リョウスケ' }
+    birthday              { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
