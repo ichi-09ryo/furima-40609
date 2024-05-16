6fc5e4c3c5bdf1784193aa2ffa0335d717c20ecc
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-16 23:38:54 +0900

    password_confirmationに対するバリデーション削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6, maximum: 255 },
                        format: { with: /\A(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)\z/ }
-  validates :password_confirmation, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
