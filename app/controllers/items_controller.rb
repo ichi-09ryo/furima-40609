983357213aeba939da7ff6d755335029d783217f
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-20 21:25:03 +0900

    items_controllerコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  def index
-    @items = Item.all
-  end
+  #def index
+    #@items = Item.all
+  #end
 
-  def show
-    @item = Item.find(params[:id])
-  end
+  #def show
+    #@item = Item.find(params[:id])
+  #end
 
   def new
     @item = Item.new
