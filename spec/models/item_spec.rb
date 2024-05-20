e2ac281b4eb23b9a126a6fcdeb080adabb9f9ac0
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-20 21:40:33 +0900

    ユーザーが紐づいていないと保存できないテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -44,6 +44,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("User must exist")
       end
 
+      it 'ユーザーが紐づいていないと保存ができない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
       it '画像がないと出品できない' do
         @item.image = nil
         @item.valid?
