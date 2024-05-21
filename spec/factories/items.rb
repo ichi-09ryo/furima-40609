e864ee30320f28c27b21fee7f21e865ab166ae49
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-21 17:29:40 +0900

    出品物表示完了

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     name { Faker::Commerce.product_name }
     description { Faker::Lorem.paragraph }
     price { Faker::Number.between(from: 300, to: 9_999_999) }
-    category_id { Faker::Number.between(from: 2, to: 11) }  # 1 is used for '---' or invalid
+    category_id { Faker::Number.between(from: 2, to: 11) } # 1 is used for '---' or invalid
     item_status_id { Faker::Number.between(from: 2, to: 7) }
     shipping_cost_id { Faker::Number.between(from: 2, to: 3) }
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
