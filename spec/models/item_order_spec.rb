dfadfde616ceffe1edaca6b8eb6fdbe371c5bdd7
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-27 03:15:14 +0900

    @userの削除

--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -2,11 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ItemOrder, type: :model do
   before do
-    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
     sleep 0.1  # 少し待機することでタイミングの問題を回避
-    @item = FactoryBot.create(:item, user: @user)
-    sleep 0.1  # 少し待機することでタイミングの問題を回避
-    @item_order = FactoryBot.build(:item_order, user_id: @user.id, item_id: @item.id)
+    @item_order = FactoryBot.build(:item_order, user_id: @item.user.id, item_id: @item.id)
     sleep 0.1  # 少し待機することでタイミングの問題を回避
   end
 
